a412e2841764a17c8ed6d6248823064f32aa5d82
Max Siz <maxsizmobile@gmail.com>
2025-07-30 17:06:37 +0700

    FEAT simple factory

--- smart-wallet/script/Deploy.s.sol
+++ smart-wallet/script/Deploy.s.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
 import {ETHGlobalSmartWallet} from "../src/ETHGlobalSmartWallet.sol";
+import {WalletFactory} from "../src/WalletFactory.sol";
 
 contract ETHGlobalSmartWalletScript is Script {
     ETHGlobalSmartWallet public impl;
+    WalletFactory public factory;
      address FACTORY;  //dummy
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-
-        impl = new ETHGlobalSmartWallet(FACTORY);
+        factory = new WalletFactory();
+        impl = new ETHGlobalSmartWallet(address(factory));
 
         vm.stopBroadcast();
     }
