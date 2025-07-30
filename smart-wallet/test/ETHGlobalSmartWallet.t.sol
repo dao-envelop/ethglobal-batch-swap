e165cac52484aac27a9c191a434279f18846c808
Alex <alexamuek@gmail.com>
2025-07-30 23:01:10 +0800

    test for executeEncodedTx

--- smart-wallet/test/ETHGlobalSmartWallet.t.sol
+++ smart-wallet/test/ETHGlobalSmartWallet.t.sol
@@ -6,16 +6,22 @@ import {Test, console} from "forge-std/Test.sol";
 import {ETHGlobalSmartWallet} from "../src/ETHGlobalSmartWallet.sol";
 import {WalletFactory} from "../src/WalletFactory.sol";
 import {Wallet} from "../src/Wallet.sol";
+import {MockERC20} from "../src/mocks/MockERC20.sol";
 
 contract ETHGlobalSmartWalletTest is Test {
     ETHGlobalSmartWallet public impl;
     WalletFactory public factory;
     ETHGlobalSmartWallet public wallet;
+    MockERC20 erc20_1;
+    MockERC20 erc20_2;
     address payable walletAddress;
+    uint256 sendERC20Amount = 2e18;
 
     function setUp() public {
         factory = new WalletFactory();
         impl = new ETHGlobalSmartWallet(address(factory));
+        erc20_1 = new MockERC20("Mock ERC20", "ERC20");
+        erc20_2 = new MockERC20("Mock ERC20", "ERC20");
         ETHGlobalSmartWallet.InitParams memory initData = ETHGlobalSmartWallet.InitParams(
             address(this),
             "ETHGlobal Smart Wallet",
@@ -57,5 +63,35 @@ contract ETHGlobalSmartWalletTest is Test {
         wallet.executeEncodedTx(ethReceiver, sendEtherAmount / 2, data);
         assertEq(ethReceiver.balance, sendEtherAmount / 2);
         assertEq(walletAddress.balance, sendEtherAmount / 2);
+        vm.prank(address(2));
+        vm.expectRevert("Only owner or keeper");
+        wallet.executeEncodedTx(ethReceiver, sendEtherAmount / 2, data);
+    }
+
+    function test_approve() public {
+        erc20_1.transfer(walletAddress, sendERC20Amount);
+        erc20_2.transfer(walletAddress, sendERC20Amount * 2);
+        assertEq(erc20_1.balanceOf(walletAddress), sendERC20Amount);
+        assertEq(erc20_2.balanceOf(walletAddress), sendERC20Amount * 2);
+        address receiver = address(2);
+        address[] memory targets = new address[](2);
+        bytes[] memory dataArray = new bytes[](2);
+        uint256[] memory values = new uint256[](2);
+        targets[0] = address(erc20_1);
+        targets[1] = address(erc20_2);
+        values[0] = 0;
+        values[1] = 0;
+        dataArray[0] = abi.encodeWithSignature("transfer(address,uint256)", receiver, sendERC20Amount);
+        dataArray[1] = abi.encodeWithSignature("transfer(address,uint256)", receiver, sendERC20Amount * 2);
+        vm.prank(receiver);
+        vm.expectRevert("Only owner or keeper");
+        wallet.executeEncodedTxBatch(targets, values, dataArray);
+        wallet.setApprovalForAll(receiver, true);
+        vm.prank(receiver);
+        wallet.executeEncodedTxBatch(targets, values, dataArray);
+        assertEq(erc20_1.balanceOf(walletAddress), 0);
+        assertEq(erc20_2.balanceOf(walletAddress), 0);
+        assertEq(erc20_1.balanceOf(receiver), sendERC20Amount);
+        assertEq(erc20_2.balanceOf(receiver), sendERC20Amount * 2);
     }
 }
