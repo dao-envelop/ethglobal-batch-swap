847a1b3b7efe3e7c68b79a119272cea42d174089
Alex <alexamuek@gmail.com>
2025-07-30 18:11:09 +0800

    use eth in wallet

--- smart-wallet/test/ETHGlobalSmartWallet.t.sol
+++ smart-wallet/test/ETHGlobalSmartWallet.t.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.30;
 
 import {Test, console} from "forge-std/Test.sol";
+//import "forge-std/console.sol";
 import {ETHGlobalSmartWallet} from "../src/ETHGlobalSmartWallet.sol";
+import {Wallet} from "../src/Wallet.sol";
 
 contract ETHGlobalSmartWalletTest is Test {
     ETHGlobalSmartWallet public impl;
@@ -15,5 +17,28 @@ contract ETHGlobalSmartWalletTest is Test {
         assertEq(impl.TOKEN_ID(), 1);
         assertEq(impl.name(), 'ETHGlobal Smart Wallet');
         assertEq(impl.symbol(), 'ETHGLW');
+    }
+
+    function test_send_eth() public {
+        uint256 sendEtherAmount = 1e18;
+        //get eth to wallet
+        vm.prank(address(this));
+        vm.expectEmit();
+        emit Wallet.EtherReceived(sendEtherAmount, sendEtherAmount, address(this));
+        (bool sent, bytes memory data) = address(impl).call{value: sendEtherAmount}("");
+        // suppress solc warnings 
+        sent;
+        data;
+        assertEq(address(impl).balance, sendEtherAmount);
+        //transfer eth from wallet
+        data = "";
+        address ethReceiver = address(2);
+        vm.prank(address(this));
+        //vm.expectEmit();
+        //emit Wallet.EtherBalanceChanged(sendEtherAmount, sendEtherAmount / 2, sendEtherAmount / 2 , ethReceiver);
+        impl.executeEncodedTx(ethReceiver, sendEtherAmount / 2, data); 
+        assertEq(ethReceiver.balance, sendEtherAmount / 2);
+        assertEq(address(impl).balance,sendEtherAmount / 2);
+
     }
 }
