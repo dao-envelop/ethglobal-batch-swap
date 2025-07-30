16907f06bb9fbdd410b86d174c7c20bc02688354
Alex <alexamuek@gmail.com>
2025-07-30 18:50:20 +0800

    use eth by wallet

--- smart-wallet/test/ETHGlobalSmartWallet.t.sol
+++ smart-wallet/test/ETHGlobalSmartWallet.t.sol
@@ -4,17 +4,35 @@ pragma solidity ^0.8.30;
 import {Test, console} from "forge-std/Test.sol";
 //import "forge-std/console.sol";
 import {ETHGlobalSmartWallet} from "../src/ETHGlobalSmartWallet.sol";
+import {WalletFactory} from "../src/WalletFactory.sol";
 import {Wallet} from "../src/Wallet.sol";
 
 contract ETHGlobalSmartWalletTest is Test {
     ETHGlobalSmartWallet public impl;
-    address FACTORY;  //dummy
+    WalletFactory factory;
+    address payable walletAddress;
+    ETHGlobalSmartWallet wallet;
 
     function setUp() public {
-        impl = new ETHGlobalSmartWallet(FACTORY);
+        factory = new WalletFactory();
+        impl = new ETHGlobalSmartWallet(address(factory));
+        ETHGlobalSmartWallet.InitParams memory initData = ETHGlobalSmartWallet.InitParams(
+            address(this),
+            'ETHGlobal Smart Wallet',
+            'ETHGLW',
+            'https://apidev.envelop.is/meta/',
+            new address[](0),
+            new bytes32[](0),
+            new uint256[](0),
+            ""
+        );
+
+        walletAddress = payable(impl.createWalletOnFactory(initData));
+        wallet = ETHGlobalSmartWallet(walletAddress);
     }
 
     function test_init() public {
+        assertEq(wallet.ownerOf(impl.TOKEN_ID()), address(this));
         assertEq(impl.TOKEN_ID(), 1);
         assertEq(impl.name(), 'ETHGlobal Smart Wallet');
         assertEq(impl.symbol(), 'ETHGLW');
@@ -26,20 +44,20 @@ contract ETHGlobalSmartWalletTest is Test {
         vm.prank(address(this));
         vm.expectEmit();
         emit Wallet.EtherReceived(sendEtherAmount, sendEtherAmount, address(this));
-        (bool sent, bytes memory data) = address(impl).call{value: sendEtherAmount}("");
+        (bool sent, bytes memory data) = walletAddress.call{value: sendEtherAmount}("");
         // suppress solc warnings 
         sent;
         data;
-        assertEq(address(impl).balance, sendEtherAmount);
+        assertEq(walletAddress.balance, sendEtherAmount);
         //transfer eth from wallet
         data = "";
         address ethReceiver = address(2);
         vm.prank(address(this));
-        //vm.expectEmit();
-        //emit Wallet.EtherBalanceChanged(sendEtherAmount, sendEtherAmount / 2, sendEtherAmount / 2 , ethReceiver);
-        impl.executeEncodedTx(ethReceiver, sendEtherAmount / 2, data); 
+        vm.expectEmit();
+        emit Wallet.EtherBalanceChanged(sendEtherAmount, sendEtherAmount / 2, 0 , address(this));
+        wallet.executeEncodedTx(ethReceiver, sendEtherAmount / 2, data); 
         assertEq(ethReceiver.balance, sendEtherAmount / 2);
-        assertEq(address(impl).balance,sendEtherAmount / 2);
+        assertEq(walletAddress.balance,sendEtherAmount / 2);
 
     }
 }
