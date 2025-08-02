f17a27343618af0d065da3da2d65785fe5d26c88
Denis Kostylev <kostydenis@gmail.com>
2025-08-02 17:05:21 +0000

    fix:revert revert for build

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -177,21 +177,3 @@ export const getStrHash = (str: string): string => {
 	const encodedStr = web3.eth.abi.encodeParameter('string', str);
 	return web3.utils.keccak256(encodedStr);
 }
-
-export const getCurrentEnvironment = () => {
-
-	const envs = [
-		'stage',
-		'alpha',
-		'beta',
-		'prod',
-	];
-
-	const domain = window.location.hostname.toLowerCase();
-
-	const foundEnv = envs.find((item) => { return domain.includes( item.toLowerCase()) });
-	if ( foundEnv ) { return foundEnv; }
-
-	return 'prod';
-
-}
